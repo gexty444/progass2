7f186886cf2a72325b0120f598810e46fe8089eb
chiseng <thehedge96@hotmail.com>
2019-04-12 16:37:20 +0800

    edited CP2

--- CP2Client.java
+++ CP2Client.java
@@ -1,5 +1,3 @@
-package ass2;
-
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
@@ -14,6 +12,7 @@ import java.net.Socket;
 import java.security.PublicKey;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.Base64;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
@@ -24,7 +23,7 @@ public class CP2Client {
     public static void main(String[] args) {
 
         String filename = "testfile.txt";
-        String filepath = "C:\\Users\\Me\\IdeaProjects\\progassig2\\src\\gapz-bootkit-whitepaper.pdf";
+        String filepath = "C:\\Users\\Me\\IdeaProjects\\progassig2\\src\\testfile.txt";
         if (args.length > 0) filename = args[0];
 
         String serverAddress = "localhost";
@@ -138,14 +137,14 @@ public class CP2Client {
 
             // Initialise RSA cipher using server's public key
             Cipher encryptCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-            encryptCipher.init(Cipher.ENCRYPT_MODE, serverPublicKey);
+            encryptCipher.init(Cipher.ENCRYPT_MODE, protocols.getServerKey());
 
             // generate a session key (AES, keysize = 128bits)
             KeyGenerator keyGen = KeyGenerator.getInstance("AES");
             keyGen.init(128);
             SecretKey sessionKey = keyGen.generateKey();
 
-            // initialise cipher using session key
+            // initialise cipher using session key for file encryption
             Cipher sessionCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             sessionCipher.init(Cipher.ENCRYPT_MODE, sessionKey);
 
@@ -154,6 +153,7 @@ public class CP2Client {
             byte[] encryptedSessionKey = encryptCipher.doFinal(sessionKeyBytes);
 
             // send encrypted session key
+            toServer.writeInt(encryptedSessionKey.length); //write to server key length
             toServer.write(encryptedSessionKey);
             toServer.flush();
             System.out.println("Sent Session Key to Server!");
@@ -163,16 +163,22 @@ public class CP2Client {
             System.out.println("1. Sending filename");
             toServer.writeInt(0);
             toServer.writeInt(filename.getBytes().length);
-            byte [] encryptedFileName=encryptCipher.doFinal(filename.getBytes());
+            byte [] encryptedFileName=sessionCipher.doFinal(filename.getBytes());
             toServer.writeInt(encryptedFileName.length);
             toServer.write(encryptedFileName);
             toServer.flush();
             System.out.println("Filename sent!");
 
             // send encrypted file
+
             File file = new File(filepath);
             byte[] fileBytes = new byte[ (int) file.length()];      // convert to byte array for encryption
+            BufferedInputStream bufins=new BufferedInputStream(new FileInputStream(file));
+            bufins.read(fileBytes, 0, (int)file.length());
+
             byte[] encryptedFile = sessionCipher.doFinal(fileBytes);    // encrypt
+            toServer.writeInt(1);
+            toServer.writeLong(fileBytes.length); //send over file size
             toServer.write(encryptedFile);
             toServer.flush();
             System.out.println("Sent File to Server!");
--- CP2Server.java
+++ CP2Server.java
@@ -1,4 +1,144 @@
-package ass2;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.PrivateKey;
 
 public class CP2Server {
+    public static void main(String[] args) {
+
+        int port = 4321;
+        if (args.length > 0) port = Integer.parseInt(args[0]);
+
+        ServerSocket welcomeSocket = null;
+        Socket connectionSocket = null;
+        DataOutputStream toClient = null;
+        DataInputStream fromClient = null;
+
+        FileOutputStream fileOutputStream = null;
+        BufferedOutputStream bufferedFileOutputStream = null;
+        ServerSide serverProtocols = new ServerSide();
+
+
+        BufferedReader bread = null;
+
+        try {
+            welcomeSocket = new ServerSocket(port);
+            connectionSocket = welcomeSocket.accept();
+            fromClient = new DataInputStream(connectionSocket.getInputStream());
+            toClient = new DataOutputStream(connectionSocket.getOutputStream());
+            BufferedReader readFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+            //TODO:Encrypt message with private key
+
+            //get private key
+            PrivateKey privateKey = serverProtocols.getPrivateKey();
+            serverProtocols.setServerPrivateKey(privateKey);
+
+            byte[] buffer = new byte[32];
+            //get our nonce from client
+            fromClient.readFully(buffer);
+            System.out.println("Got nonce from client");
+
+
+            //TODO:encrypt nonce
+            Cipher encryptMessageToClient = Cipher.getInstance("RSA");
+            encryptMessageToClient.init(Cipher.ENCRYPT_MODE, privateKey);
+            byte[] encryptednonce = encryptMessageToClient.doFinal(buffer);
+            toClient.writeInt(encryptednonce.length);
+            System.out.println("Nonce length transferred");
+            toClient.flush();
+            toClient.write(encryptednonce);
+            System.out.println("Nonce transferred");
+            toClient.flush();
+
+            String certreq = readFromClient.readLine();
+
+            //TODO:send certificate
+            if (certreq.equals("Give me your certificate!")) {
+                System.out.println("Preparing cert");
+                File certificate = new File("C:\\Users\\Me\\IdeaProjects\\progassig2\\src\\example.org.crt");
+                byte[] certByte = new byte[(int) certificate.length()];
+                try {
+                    FileInputStream fis = new FileInputStream(certificate);
+                    fis.read(certByte);
+                    fis.close();
+
+                } catch (IOException ioExp) {
+                    ioExp.printStackTrace();
+                }
+                toClient.writeInt(certByte.length);
+                toClient.flush();
+                toClient.write(certByte);
+                toClient.flush();
+                System.out.println("Cert sent");
+            }
+            ServerSide sprotocols = new ServerSide();
+//			byte[] newChallenge=sprotocols.createChallenge();
+
+            // receive message from client (whether AP passed or failed)
+            String APresult = readFromClient.readLine();
+            if (APresult.equals("Bye, you liar")) {
+                // we stop, close all connections
+                fromClient.close();
+                toClient.close();
+                readFromClient.close();
+                connectionSocket.close();
+            }
+
+            //Get session key and decrypt
+            int length = fromClient.readInt();
+            byte[] encryptedSessionKey = new byte[length];
+            fromClient.readFully(encryptedSessionKey, 0, length);//read the session key
+            System.out.println("Received Session Key");
+            Cipher decryptKey = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            decryptKey.init(Cipher.DECRYPT_MODE, serverProtocols.getPrivateKey());
+            byte[] byteSessionKey = decryptKey.doFinal(encryptedSessionKey);
+            SecretKey sessionKey = new SecretKeySpec(byteSessionKey, "AES");
+
+
+            //Receive file from client
+            while (!connectionSocket.isClosed()) {
+                int packetType = fromClient.readInt();
+                if (packetType == 0) {
+                    System.out.println("Receiving file...");
+
+                    System.out.println("1. Receiving encrypted filename");
+                    int numBytes = fromClient.readInt();//numbytes for decrypted filename is different from encrypted
+                    int encryptedFileHeaderLength = fromClient.readInt(); //get length
+                    byte[] encryptedFilename = new byte[encryptedFileHeaderLength];
+                    fromClient.readFully(encryptedFilename, 0, encryptedFileHeaderLength); //get encrypted file name
+                    System.out.println("Received filename!");
+
+                    //Decrypt file header
+                    Cipher decryptFileName = Cipher.getInstance("AES/ECB/PKCS5Padding");
+                    decryptFileName.init(Cipher.DECRYPT_MODE, sessionKey);
+                    byte[] decryptedFileName = decryptFileName.doFinal(encryptedFilename);
+                    fileOutputStream = new FileOutputStream("C:\\Users\\Me\\Documents\\" + new String(decryptedFileName, 0, numBytes));
+                    bufferedFileOutputStream = new BufferedOutputStream(fileOutputStream);
+                    System.out.println("File header Decrypted");
+
+
+                } else if (packetType == 1) {
+                    System.out.println("2. Reading client output");
+                    long filelength = fromClient.readLong();
+                    byte[] encryptedFile = new byte[(int) filelength];
+                    fromClient.readFully(encryptedFile, 0, (int) filelength);
+                    System.out.println("Received client output");
+                    System.out.println("Decrypting file...");
+                    Cipher decryptFileName = Cipher.getInstance("AES/ECB/PKCS5Padding");
+                    decryptFileName.init(Cipher.DECRYPT_MODE, sessionKey);
+
+                    byte[] decryptedBlock = decryptFileName.doFinal(encryptedFile);
+                    System.out.println("Writing to file");
+                    bufferedFileOutputStream.write(decryptedBlock, 0, (int) filelength);
+                }
+            }
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
 }
